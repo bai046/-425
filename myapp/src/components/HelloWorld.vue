3efd18537548391f0c48c0679c1771d53bbb9150
shuying Guo <70828681+XiaoBai918@users.noreply.github.com>
2021-04-27 08:14:28 +0800

    vue+vue-Axios+Express实现简单跨域访问

---
+++ myapp/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <button type="button" @click="response">Click Me!</button>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  name: "login",
+  methods:{
+    response(){
+      // alter(123);
+      Axios.get('http://localhost:3000/users').then(function(response){
+        console.log("你已连接！");
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
